b0c3b5c31deb1dc2b90c54bf8cc9afb88d22af50
Alper Said Soylu <54alpersaid@gmail.com>
2023-02-15 23:16:07 +0300

    udpate

--- python/main.py
+++ python/main.py
@@ -1,5 +1,7 @@
 import sys
 import proto.account_pb2
+import proto.user_pb2
+import proto.product_pb2
 
 def account():
     return proto.account_pb2.Account(
@@ -9,9 +11,25 @@ def account():
         follow_ids=[0, 1],
     )
 
+def user():
+    u = proto.user_pb2.User()
+    u.id = 42
+    u.name = 'Linux Torvalds'
+    u.follows.add(id=0, name='Linux Foundation')
+    return u
+
+def product():
+    return proto.product_pb2.Product(
+        id = 42,
+        type = proto.product_pb2.Product.Type.PANTS,
+    )
+
+
 
 if __name__ == "__main__":
     fns = {
-        'account': account
+        'account': account,
+        'user': user,
+        'product': product,
     }
     print(fns[sys.argv[1]]())
